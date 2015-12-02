3e7b0953ff2c1f88c6d25b9ecdcdf4bc190d36fa
tnrich <trich1230@gmail.com>
2015-12-02 10:24:37 -0800

    creating personal react component starter

---
+++ app.js
@@ -0,0 +1,9 @@
+/*!
+ * React Component Starter Kit
+ * https://github.com/tnrich/react-component-starter
+ * Copyright (c) tnrich, LLC. All rights reserved. See LICENSE.txt
+ */
+
+import React from 'react';
+import Component from './src';
+React.render(<Component />, document.getElementById('component'));
